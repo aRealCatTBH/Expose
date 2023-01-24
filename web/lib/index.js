6e5a1b3a357844a01c70d3a8f3780ffe52fcd67c
Cat <89495053+aRealCatTBH@users.noreply.github.com>
2023-01-24 13:27:51 -0500

    Update index.js

--- web/lib/index.js
+++ web/lib/index.js
@@ -4,6 +4,9 @@ window.ExposePresence = {
         state: "",
         largeImageKey: "",
     },
+    richPresence: {
+        init: () => this.ws.send(ExposePresence.presData),
+    },
     github: {
         getRepo: location.pathname.replace("/", ""),
     },
