c0e82005b439418a87f24f33db848d4a036c8a43
Cat <89495053+aRealCatTBH@users.noreply.github.com>
2023-01-24 16:52:49 -0800

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+let { WebSocketServer } = require("ws")
+let wss = new WebSocketServer({ port: 1615 });
+let rpc = require("discord-rpc");
+rpc.register("1067582647745925221");
+rpc = new rpc.Client({ transport: 'ipc' });
 
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data) {
+        rpc.setActivity(JSON.parse(data));
+    });
+
+    rpc.login({ clientId: "1067582647745925221" }).catch(console.error);
+});
